b81b780b123cb92af996fc434a8faf14f6a59531
Richard Pitts <richardepitts@gmail.com>
2019-08-04 01:22:38 +0000

    Automatically backed up by Learn

---
+++ Practice.js
@@ -0,0 +1,12 @@
+function johnLennonFacts(facts_array) {
+  for (i=0; i < johnLennonFacts.length; i++) {
+    console.log(`{i+1} + ":" + facts_array[i] + "!!!"`)
+  }
+}
+
+function johnLennonFacts(array) {
+  i = 0 
+  while (i < array.length)
+  array[i] += 1
+  i++
+}
